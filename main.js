2c79281416dfe500daf4873d2fb8b71d10382db5
Belart3 <enochbelawu@gmail.com>
2022-06-14 14:52:37 +0100

    4belat

---
+++ main.js
@@ -0,0 +1,13 @@
+//  alert("helloo!! welcome to my page");
+
+//  var heds = document.querySelector('.heds');
+//  var art = document.querySelector('.art');
+
+// heds.removeChild( art);
+
+//  var ourDiv = document.getElementsByClassName("heds");
+//  var swapMe = document.getElementsByClassName("art");
+//  var newImg = document.createElement("img"); 
+
+//  newImg.setAttribute( "src", "undraw.png" );
+// ourDiv.replaceChild( newImg, swapMe );
